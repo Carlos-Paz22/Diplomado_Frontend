f74ce7df9c5727417024286eab9692fefa0379c5
Franklin-Molina <asdfglxl7@gmail.com>
2020-12-08 02:02:11 -0500

    diseño categorias

--- src/views/Users/Myperfil.vue
+++ src/views/Users/Myperfil.vue
@@ -1,30 +1,6 @@
 <template>
   <div>
-    <b-navbar toggleable="md" type="dark" class="Nav">
-      <b-navbar-brand href="/">Inicio</b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <!-- <b-navbar-nav>
-        
-        <b-nav-item href="/registro">Registro</b-nav-item>
-         <b-nav-item href="/home" >Categorias</b-nav-item>
-      </b-navbar-nav> -->
-
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template #button-content>
-              <em>Mi Perfil</em>
-            </template>
-
-            <b-dropdown-item @click="salir">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+  
 
     <div class="container-flex">
       <b-carousel
